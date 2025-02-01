cf054951153c6fc75accd19b6c7e87ac2ac7134b
Zi Xuan <81336752+zixuanvtzx@users.noreply.github.com>
2025-02-02 03:17:36 +0800

    Update network.py

--- network.py
+++ network.py
@@ -3,12 +3,10 @@ import ast
 
 def read_column_from_csv(file_path):
     try:
-        # Open the CSV file
         with open(file_path, 'r') as file:
-            # Create a CSV reader object
             csvreader = csv.reader(file)
             header = next(csvreader)
-            # Extract the column data
+            
             column_data = []
             for row in csvreader:
                 actual_list = ast.literal_eval(row[2])
@@ -19,11 +17,9 @@ def read_column_from_csv(file_path):
     except Exception as e:
         return str(e)
 
-# Example usage
-file_path = 'insights_results.csv'  # Provide the path to your CSV file
+file_path = 'insights_results.csv'
 
 column_data = read_column_from_csv(file_path)
-#print(column_data)
 
 network = []
 for row in column_data:
@@ -43,10 +39,8 @@ import os
 # Select the first 10 data points from the dataset
 sampled_data = network[:10]
 
-# Create a NetworkX graph
 G = nx.Graph()
 
-# Add nodes and edges from the sampled data
 for entities in sampled_data:
     for i in range(len(entities)):
         for j in range(i + 1, len(entities)):
@@ -54,18 +48,14 @@ for entities in sampled_data:
             entity1, entity2 = entities[i][0], entities[j][0]
             G.add_edge(entity1, entity2)
 
-# Create a Pyvis network graph for visualization
 net = Network(notebook=True, height="600px", width="100%")
 net.from_nx(G)
 
-# Save the graph as an HTML file
 html_path = "sample_graph.html"
 net.save_graph(html_path)
 
-# Display the network graph in Streamlit using the HTML file
 st.write("### Network Graph of Entities and Relationships (First 10 Data Points)")
 with open(html_path, "r", encoding="utf-8") as f:
     st.components.v1.html(f.read(), height=600)
 
-# Clean up the generated HTML file after displaying
 os.remove(html_path)
